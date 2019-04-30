d0afeacbf1a15b051ba7082b6b65f0fe77365a25
Stephen Mathieson <me@stephenmathieson.com>
2019-04-30 16:02:16 -0400

    move test json into testdata dir

--- navjson/navjson_test.go
+++ navjson/navjson_test.go
@@ -7,83 +7,40 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func readTestdataFile(filename string) (string, error) {
+	data, err := ioutil.ReadFile("./testdata/" + filename)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
+
 func TestParse(t *testing.T) {
 	t.Run("full valid doc", func(t *testing.T) {
-		j, err := Parse(`
-			{
-				"root": "guide/attest/2.7-experiment/",
-				"assetRoot": "assets/images/attest/2.7-experiment/",
-				"skipMenuOrdering": true,
-				"packages": {
-					"attest-js": "path",
-					"attest-puppeteer": "path"
-				},
-				"files": [
-					{
-						"path": "api-integrations",
-						"files": [
-							{
-								"name": "attest-js/browser-js.html",
-								"path": "browser-js"
-							},
-							{
-								"name": "attest-puppeteer/index.html",
-								"path": "node-integrations",
-								"files": [
-									{
-										"name": "attest-puppeteer/attest-puppeteer.html",
-										"path": "attest-puppeteer"
-									}
-								]
-							}
-						]
-					}
-				]
-			}
-		`)
+		data, err := readTestdataFile("simple-valid.json")
+		assert.NoError(t, err)
 
+		j, err := Parse(data)
 		assert.NoError(t, err)
 		assert.NotNil(t, j)
 		assert.Equal(t, j.Root, "guide/attest/2.7-experiment/")
 	})
 
 	t.Run("valid doc no packages", func(t *testing.T) {
-		j, err := Parse(`
-			{
-				"root": "guide/attest/2.7-experiment/",
-				"assetRoot": "assets/images/attest/2.7-experiment/",
-				"skipMenuOrdering": true,
-				"files": [
-					{
-						"path": "api-integrations"
-					}
-				]
-			}
-		`)
+		data, err := readTestdataFile("no-packages.json")
+		assert.NoError(t, err)
 
+		j, err := Parse(data)
 		assert.NoError(t, err)
 		assert.NotNil(t, j)
 		assert.Nil(t, j.Packages)
 	})
 
 	t.Run("packages", func(t *testing.T) {
-		j, err := Parse(`
-			{
-				"root": "guide/attest/2.7-experiment/",
-				"assetRoot": "assets/images/attest/2.7-experiment/",
-				"skipMenuOrdering": true,
-				"packages": {
-					"attest-js": "path",
-					"attest-puppeteer": "path"
-				},
-				"files": [
-					{
-						"path": "api-integrations"
-					}
-				]
-			}
-		`)
+		data, err := readTestdataFile("with-packages.json")
+		assert.NoError(t, err)
 
+		j, err := Parse(data)
 		assert.NoError(t, err)
 		assert.NotNil(t, j)
 		assert.NotNil(t, j.Packages)
@@ -91,20 +48,10 @@ func TestParse(t *testing.T) {
 	})
 
 	t.Run("files with name", func(t *testing.T) {
-		j, err := Parse(`
-			{
-				"root": "guide/attest/2.7-experiment/",
-				"assetRoot": "assets/images/attest/2.7-experiment/",
-				"skipMenuOrdering": true,
-				"files": [
-					{
-						"path": "api-integrations",
-						"name": "hello"
-					}
-				]
-			}
-		`)
+		data, err := readTestdataFile("files-with-names.json")
+		assert.NoError(t, err)
 
+		j, err := Parse(data)
 		assert.NoError(t, err)
 		assert.NotNil(t, j)
 		assert.Len(t, j.Files, 1)
@@ -119,56 +66,34 @@ func TestParse(t *testing.T) {
 	})
 
 	t.Run("missing root", func(t *testing.T) {
-		_, err := Parse(`
-			{
-				"assetRoot": "assets/images/attest/2.7-experiment/",
-				"files": [
-					{
-						"path": "api-integrations"
-					}
-				]
-			}
-		`)
+		data, err := readTestdataFile("missing-root.json")
+		assert.NoError(t, err)
+
+		_, err = Parse(data)
 		assert.Error(t, err)
 	})
 
 	t.Run("missing assetRoot", func(t *testing.T) {
-		_, err := Parse(`
-			{
-				"root": "guide/attest/2.7-experiment/",
-				"files": [
-					{
-						"path": "api-integrations"
-					}
-				]
-			}
-		`)
+		data, err := readTestdataFile("missing-assetRoot.json")
+		assert.NoError(t, err)
+
+		_, err = Parse(data)
 		assert.Error(t, err)
 	})
 
 	t.Run("missing files", func(t *testing.T) {
-		_, err := Parse(`
-			{
-				"root": "guide/attest/2.7-experiment/",
-				"assetRoot": "assets/images/attest/2.7-experiment/"
-			}
-		`)
+		data, err := readTestdataFile("missing-files.json")
+		assert.NoError(t, err)
+
+		_, err = Parse(data)
 		assert.Error(t, err)
 	})
 
 	t.Run("skipMenuOrdering as string", func(t *testing.T) {
-		_, err := Parse(`
-			{
-				"root": "guide/attest/2.7-experiment/",
-				"assetRoot": "assets/images/attest/2.7-experiment/",
-				"skipMenuOrdering": "true",
-				"files": [
-					{
-						"path": "api-integrations"
-					}
-				]
-			}
-		`)
+		data, err := readTestdataFile("invalid-skipMenuOrdering.json")
+		assert.NoError(t, err)
+
+		_, err = Parse(data)
 		assert.Error(t, err)
 	})
 }
@@ -177,9 +102,9 @@ func TestParseExamples(t *testing.T) {
 	files := []string{"attest-docs.json", "attest-node-suite.json"}
 	for _, filename := range files {
 		t.Run(filename, func(t *testing.T) {
-			data, err := ioutil.ReadFile("./testdata/" + filename)
+			data, err := readTestdataFile(filename)
 			assert.NoError(t, err)
-			_, err = Parse(string(data))
+			_, err = Parse(data)
 			assert.NoError(t, err)
 		})
 	}
@@ -187,48 +112,14 @@ func TestParseExamples(t *testing.T) {
 
 func TestIsValid(t *testing.T) {
 	t.Run("valid", func(t *testing.T) {
-		data := `
-			{
-				"root": "guide/attest/2.7-experiment/",
-				"assetRoot": "assets/images/attest/2.7-experiment/",
-				"skipMenuOrdering": true,
-				"packages": {
-					"attest-js": "path",
-					"attest-puppeteer": "path"
-				},
-				"files": [
-					{
-						"path": "api-integrations",
-						"files": [
-							{
-								"name": "attest-js/browser-js.html",
-								"path": "browser-js"
-							},
-							{
-								"name": "attest-puppeteer/index.html",
-								"path": "node-integrations",
-								"files": [
-									{
-										"name": "attest-puppeteer/attest-puppeteer.html",
-										"path": "attest-puppeteer"
-									}
-								]
-							}
-						]
-					}
-				]
-			}
-		`
+		data, err := readTestdataFile("simple-valid.json")
+		assert.NoError(t, err)
 		assert.True(t, IsValid(data))
 	})
 
 	t.Run("invalid", func(t *testing.T) {
-		data := `
-			{
-				"root": "guide/attest/2.7-experiment/",
-				"assetRoot": "assets/images/attest/2.7-experiment/",
-			}
-		`
+		data, err := readTestdataFile("missing-files.json")
+		assert.NoError(t, err)
 		assert.False(t, IsValid(data))
 	})
 }
